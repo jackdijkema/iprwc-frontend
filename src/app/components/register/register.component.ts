c00bcd37bdb6b44d7e7ab7b56c39bd5e334143a3
Jack <jackdijkema@hotmail.com>
2024-03-13 22:20:04 +0100

    [FEAT] Add register component styling

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
+  imports: [NavbarComponent],
 })
-export class RegisterComponent {
-
-}
+export class RegisterComponent { }
