2e7cd8e08e26cf858c6fe2151e8df7c6a5177508
Jack <jackdijkema@hotmail.com>
2024-03-15 22:02:05 +0100

    [FEAT] Add logout button + hide login button when loggedin

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  loggedIn(): boolean {
+    if (this.authService.isAuthenticated()) {
+      return true;
+    }
+    return false;
+  }
+
+  logout(): void {
+    if (this.authService.getToken()) return this.authService.removeToken();
+    this.router.navigate(['/login']);
+  }
+}
